75782489b0af52bc55778a1808209940f263d30a
rishabhm74 <42689578+rishabhm74@users.noreply.github.com>
2020-03-29 21:05:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function runner(){
+    document.getElementById('main_top_container').style.transform = "translateY(50vh)";
+    document.getElementById('main_top_2_container').style.transform = "translateY(90vh)";
+}
+
+function show_nav(){
+    document.getElementById('new_navbar').style.transform = "translateY(13vh)";
+}
+
+function close_nav(){
+    document.getElementById('new_navbar').style.transform = "translateY(-13vh)";
+}
